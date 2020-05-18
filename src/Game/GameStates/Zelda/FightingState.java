19666056fa9f901bca37f82cc64f11a944eeef4f
ashth <ashth@DESKTOP-CLD85LD.lan>
2020-05-17 22:59:51 -0400

    Updated Fighting state with new functions

--- src/Game/GameStates/Zelda/FightingState.java
+++ src/Game/GameStates/Zelda/FightingState.java
@@ -1,6 +1,7 @@
 package Game.GameStates.Zelda;
 
 import Game.GameStates.State;
+import Game.Zelda.Entities.Dynamic.BossFight;
 import Game.Zelda.Entities.Dynamic.LinkFight;
 import Main.Handler;
 import Resources.Animation;
@@ -16,17 +17,18 @@ public class FightingState extends State {
 	public boolean enter = false, runOnce = false;
 	public int size = 24, choice = 0, stealingCount = 60*2, stealingCount2 = 60*2, caser = 0, musicCounter = 0;
 	public String shopMode = "Choices";
-	public Rectangle floor = new Rectangle(130, handler.getHeight()/2 + 30 ,handler.getWidth()/2 + 300,10);;
+	public Rectangle floor = new Rectangle(130, handler.getHeight()/2 + 30 ,handler.getWidth()/2 + 600,10);;
     public FightingState(Handler handler) {
         super(handler);
     }
     
-    LinkFight linkFight = new LinkFight(150, handler.getHeight()/2 - 200, Images.linkFight, handler);
+    public LinkFight linkFight = new LinkFight(150, handler.getHeight()/2 - 200, Images.linkFight, handler);
+    public BossFight ganonFight = new BossFight(handler.getHeight(), handler.getHeight()/2 - 200, Images.ganonFight, handler);
     
     @Override
     public void tick() {
     	linkFight.tick();
-    	
+    	ganonFight.tick();
     	if (!runOnce) {
     		runOnce = true;
     		handler.getMusicHandler().changeMusic("FinalBattle2d.wav");
@@ -39,7 +41,7 @@ public class FightingState extends State {
     	g.drawImage(Images.linkFight[7],0 ,0, handler.getWidth(), handler.getHeight(), null);
     	g.drawRect(floor.x, floor.y, floor.width, floor.height);
     	linkFight.render(g);
-    	
+    	ganonFight.render(g);
     	
     	g.setFont(new Font("TimesRoman", Font.PLAIN, size));
     	
