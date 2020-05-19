f023fb239a6e32bd575227e746b75e26d5ec0910
EithanCapella <60627671+EithanCapella@users.noreply.github.com>
2020-05-19 01:17:56 -0400

    Readded image for gameOverState

--- src/Game/GameStates/Zelda/GameOverState.java
+++ src/Game/GameStates/Zelda/GameOverState.java
@@ -53,29 +53,10 @@ public class GameOverState extends State {
 	public void render(Graphics g) { 	    
 	
 		
-		//g.drawImage(Images.gameOverScreen, handler.getWidth()/3, handler.getHeight()/3, null);
+		g.drawImage(Images.gameOverScreen, handler.getWidth()/3, handler.getHeight()/3, null);
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("TimesRoman", Font.ROMAN_BASELINE, 48));
 		g.drawString("--Continue--", handler.getWidth()/2 - 150, handler.getHeight()/2 + 100);
-		
-//		switch (caser) {//switches between the message displayed on the game over screen
-//
-//		case 0:
-//			g.drawString("Waka waka waka, waka waka. Waka waka waka <Waka Waka! Waka...>", handler.getWidth()/6, handler.getHeight()/5);
-//			break;
-//
-//		case 1:
-//			g.drawString("At least you do not have to pay a quarter.", handler.getWidth()/4, handler.getHeight()/5);
-//			break;
-//
-//		case 2:
-//			g.drawString("Pac-Man... It's time to talk about your pill addiction.", handler.getWidth()/4, handler.getHeight()/5);
-//			break;
-//
-//		case 3:
-//			g.drawString("Ms.Pac-Man is dissapointed in you", handler.getWidth()/3, handler.getHeight()/5);
-//			break;
-//		}
 	}
 
 
@@ -91,7 +72,9 @@ public class GameOverState extends State {
 		handler.getZeldaGameState().link.hasBow = false;
 		handler.getZeldaGameState().link.hasRaft = false;
 		handler.getZeldaGameState().link.hasRod = false;
+		handler.getZeldaGameState().refresh();
 		handler.getMusicHandler().stopMusic();
 		handler.getMusicHandler().changeMusic("nature.wav");
 	}
 }
+
